b6d59d852271a1e5c9aeb6d478a680e75b1d6793
hungdonguyen <167590894+hungdonguyen@users.noreply.github.com>
2025-12-05 23:42:27 +0700

    update gitnore

--- TestThiBackEnd/obj/Debug/net7.0/TestThiBackEnd.AssemblyInfo.cs
+++ TestThiBackEnd/obj/Debug/net7.0/TestThiBackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestThiBackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9178894f02cfd75fd93ef3c8a73fc705b24d56d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestThiBackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestThiBackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
